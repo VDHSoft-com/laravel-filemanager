0fa4591a45242b0f467924b0cacc4dc0426a29f2
VDHSoft-com <139960941+VDHSoft-com@users.noreply.github.com>
2024-09-05 17:35:28 +0200

    Update FilemanagerLaravelServiceProvider.php

--- src/FilemanagerLaravelServiceProvider.php
+++ src/FilemanagerLaravelServiceProvider.php
@@ -4,56 +4,52 @@ namespace VDHSoft\FilemanagerLaravel;
 
 use Illuminate\Support\ServiceProvider;
 
-class FilemanagerLaravelServiceProvider extends ServiceProvider {
-
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		// $this->package('VDHSoft/FilemanagerLaravel');
-	}
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-        $this->app->singleton('FilemanagerLaravel', function () {
-            return new FilemanagerLaravel;
-        });
-		$this->app->booting(function()
-		{
-		  $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-		  $loader->alias('FilemanagerLaravel', 'VDHSoft\FilemanagerLaravel\Facades\FilemanagerLaravel');
-		});
-		$this->publishes([
+class FilemanagerLaravelServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Publier les ressources
+        $this->publishes([
             __DIR__ . '/../../controllers' => base_path('app/Http/Controllers'),
             __DIR__ . '/../../views' => base_path('resources/views/vendor/filemanager-laravel'),
             __DIR__ . '/../../../public' => base_path('public'),
-        ]);
-        $this->loadViewsFrom(__DIR__ . 'resources/views', 'filemanager-laravel');
-	}
+        ], 'filemanager-laravel');
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('FilemanagerLaravel');
-	}
+        // Charger les vues
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'filemanager-laravel');
 
+        // Alias (si nécessaire)
+        $this->app->booting(function () {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('FilemanagerLaravel', 'VDHSoft\FilemanagerLaravel\Facades\FilemanagerLaravel');
+        });
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('FilemanagerLaravel', function () {
+            return new FilemanagerLaravel;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['FilemanagerLaravel'];
+    }
 }
+
